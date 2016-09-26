0e652e06e77a4f1fcc98e9a9623db36ddf231b3c
Denise Kwok <dekwok@microsoft.com>
2016-09-26 15:20:50 -0700

    Updated font

--- javascripts/mailto.js
+++
@@ -1,6 +0,0 @@
-<script language="JavaScript"><!--
-var name = "protected";
-var domain = "cdrsoft.com";
-document.write('<a href=\"mailto:' + name + '@' + domain + '\">');
-document.write(name + '@' + domain + '</a>');
-// --></script>
